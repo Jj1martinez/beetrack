3de403cac7633d180d806e3c92c5aec5063625b8
Jj1martinez <juan@MacBook-Pro-de-Juan.local>
2020-06-10 14:17:51 -0400

    Api connection Done, backend and frontend

--- app/javascript/components/Show.js
+++ app/javascript/components/Show.js
@@ -1,16 +1,71 @@
-import React from "react"
-import PropTypes from "prop-types"
-class Show extends React.Component {
-  render () {
-    return (
-      <React.Fragment>
-        Hola hola
-      </React.Fragment>
-    );
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import { makeStyles } from '@material-ui/core/styles';
+import {Grid} from '@material-ui/core';
+
+const useStyles = makeStyles({
+  root: {
+    background: 'linear-gradient(0deg, blue 40%, yellow 90%)',
+    backgroundSize: "cover",
+    height: "100%"
+  },
+  container:{
+    padding:150,
+    paddingBottom:500,
+    textAlign:"center",
+  },
+  content:{
+    backgroundColor: "#D2D7DF",
+    padding:10,
+    borderRadius: 5,
+  },
+  grid:{
+    height: '100%',
+    paddingTop: 25,
   }
+});
+
+
+const Show = () => {
+    const [carsWaypoints, setCarsWaypoints] = useState([]);
+    const classes = useStyles();
+
+
+    useEffect(()=>{
+      axios.get('/show')
+      .then(res => {
+        console.log(res)
+        setCarsWaypoints(res.data)
+      })
+    },[])
+
+    return (
+    <div className= {classes.root}>
+      <div className={classes.container}>
+        <div className={classes.content}>
+        <h1>Latest Routes</h1>
+        <Grid className={classes.grid}
+        container
+        >
+        <Grid
+          item
+          lg={8}
+        >
+         Live map
+        </Grid>
+        <Grid
+          
+          item
+          lg={4}
+        >
+          Generate new Waypoint
+        </Grid>
+
+        </Grid>
+        </div>
+      </div>
+    </div>
+    )
 }
 
-Show.propTypes = {
-  greeting: PropTypes.string
-};
 export default Show

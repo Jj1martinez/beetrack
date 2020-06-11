b06fa97c463a0d0ba5e9d9e4baece5635a8072a3
Jj1martinez <juan@MacBook-Pro-de-Juan.local>
2020-06-11 17:46:59 -0400

    Feature: Code is documented

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  # 1. Rutas para el lado del cliente
+
   root 'homepage#index'
   get 'show', to: 'homepage#show'
-  #Add Waypoint route
+
+  # 2. Rutas para nuestra API
+  # 2.1 Endpoint donde se hace el post
   post 'api/v1/gps', to: 'waypoints#add'
+  # 2.2 Endpoint se consulta los Waypoints actuales
   get 'get_waypoints', to: 'waypoints#show'
 end
